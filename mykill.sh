01defb18dd6898f9eeb0d4f94a475d69370714ae
NASDAMI Quatadah <Quatadah.Nasdami@bordeaux-inp.fr>
2020-12-30 12:06:42 +0100

    Calculator

--- mykill.sh
+++ mykill.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+#necessary condition since the user should enter at least one parameter (the program sees the first one)
+if [ $# -eq 0 ]
+then
+    echo "Usage:$0 No parameter has been entered"
+    exit 1
+fi
+
 fileToKill=$1
 pids=$(ps | grep $fileToKill | awk '{print $1}')
 
